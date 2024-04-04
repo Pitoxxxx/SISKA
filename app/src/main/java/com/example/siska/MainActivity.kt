3aa57e15c7df44c4a5f9fada638e0aedf5c8037d
adriza163 <adrizarafly03@gmial.com>
2024-04-04 14:49:23 +0700

    ok

--- app/src/main/java/com/example/siska/MainActivity.kt
+++ app/src/main/java/com/example/siska/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.example.siska
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
-import android.webkit.PermissionRequest
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
-import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     private val FILECHOOSER_RESULTCODE = 1
     private var mUploadMessage: ValueCallback<Array<Uri>>? = null
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
+    // Mendefinisikan LocationManager
+    private lateinit var locationManager: LocationManager
+
+    companion object {
+        // Inisialisasi waktu minimum antara pembaruan lokasi (dalam milidetik)
+        private const val MIN_TIME_BW_UPDATES: Long = 1000 * 60 * 1 // 1 minute
+
+        // Inisialisasi jarak minimum antara pembaruan lokasi (dalam meter)
+        private const val MIN_DISTANCE_CHANGE_FOR_UPDATES: Float = 10F // 10 meters
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +50,74 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        // Inisialisasi LocationManager
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Cek apakah izin lokasi sudah diberikan
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Jika belum, minta izin
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            // Izin lokasi sudah diberikan, lanjutkan aktivitas selanjutnya
+            // contoh: muat WebView
+            loadWebView()
+            // Dapatkan lokasi pengguna
+            getLocation()
+        }
+    }
+
+    // Handling the result of the file chooser activity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data?.data?.let { arrayOf(it) }
+                mUploadMessage?.onReceiveValue(result)
+                mUploadMessage = null
+            } else {
+                mUploadMessage?.onReceiveValue(null)
+                mUploadMessage = null
+            }
+        }
+    }
+
+    // Tangani respons izin dari pengguna
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Izin diberikan, lanjutkan aktivitas selanjutnya
+                    // contoh: muat WebView
+                    loadWebView()
+                    // Dapatkan lokasi pengguna
+                    getLocation()
+                } else {
+                    // Izin ditolak, beri tahu pengguna tentang keterbatasan aplikasi
+                    Toast.makeText(
+                        this,
+                        "Aplikasi memerlukan izin lokasi untuk bekerja dengan baik",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun loadWebView() {
+        // Lakukan semua inisialisasi WebView dan muat URL di sini
         val webView: WebView = findViewById(R.id.WV)
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://siska.kimiafarma.co.id/")
@@ -66,19 +154,55 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Handling the result of the file chooser activity
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == FILECHOOSER_RESULTCODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                val result = data?.data?.let { arrayOf(it) }
-                mUploadMessage?.onReceiveValue(result)
-                mUploadMessage = null
-            } else {
-                mUploadMessage?.onReceiveValue(null)
-                mUploadMessage = null
+    // Mendapatkan lokasi pengguna
+    private fun getLocation() {
+        // Cek apakah GPS atau jaringan tersedia
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+            )
+        ) {
+            // Request pembaruan lokasi
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
             }
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MIN_TIME_BW_UPDATES,
+                MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                locationListener
+            )
+        } else {
+            // GPS atau jaringan tidak tersedia, tampilkan pesan kepada pengguna
+            Toast.makeText(
+                this,
+                "GPS atau jaringan tidak tersedia, aktivkan untuk mendapatkan lokasi",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
+    // Mendefinisikan listener untuk mendengarkan perubahan lokasi
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            // Lokasi pengguna berubah, Anda bisa menggunakan data lokasi di sini
+            val latitude = location.latitude
+            val longitude = location.longitude
+
+            // Misalnya, Anda dapat menampilkan lokasi pengguna dalam Logcat
+            Log.d("Location", "Latitude: $latitude, Longitude: $longitude")
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+        override fun onProviderEnabled(provider: String) {}
+
+        override fun onProviderDisabled(provider: String) {}
+    }
 }
